0c8f3999eb8e3ddfd4d363c18b87c57417667038
Farjan Hasan <farjanhasannias@gmail.com>
2018-07-06 21:33:40 +0600

    Initial commit.

---
+++ src/helloproject/HelloProject.java
@@ -0,0 +1,17 @@
+package helloproject;
+
+/**
+ *
+ * @author ITPAL ACCOUNTS
+ */
+public class HelloProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+
+}
